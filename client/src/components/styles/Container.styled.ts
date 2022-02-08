9e6e3f6f8d78adcadd7e81c14c27dbc902ba8250
AviadSofer <aviadsofer@gmail.com>
2022-02-08 23:54:15 +0200

    fix resposive

--- client/src/components/styles/Container.styled.ts
+++ client/src/components/styles/Container.styled.ts
@@ -18,11 +18,15 @@ flex-basis: 80%;
 justify-content: space-between;
 margin-top: 20px;
 margin-bottom: 30px;
+@media (max-width: 768px) {
+    justify-content: space-around;
+}
 `
 
 export const Title = styled.h1`
 font-size: 1.6rem;
 color: black;
+margin: 0;
 `
 
 export const Explain = styled.h2`
@@ -32,4 +36,7 @@ font-size: 1rem;
 export const FlexContainer = styled.div`
 display: flex;
 justify-content: space-between;
+@media (max-width: 768px) {
+    margin-bottom: 100px;
+}
 `

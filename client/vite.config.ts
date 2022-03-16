798f1fc462844c0d360c6aa6daa437609ba3a6cc
AviadSofer <aviadsofer@gmail.com>
2022-03-16 13:25:36 +0200

    lint vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: true,
-    proxy: {'/api/messages': 'http://localhost:5000/'}
+    proxy: { '/api/messages': 'http://localhost:5000/' },
   },
   plugins: [react()],
-  
-})
+
+});

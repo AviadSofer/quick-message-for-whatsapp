1d862af77078cfced8b2a0e4b217f1fd39eb2f5e
AviadSofer <aviadsofer@gmail.com>
2022-03-29 11:22:26 +0300

    update the tests name, and make MessageView.context.test.tsx more readble

--- client/src/tests/MessageView.context.test.tsx
+++ client/src/tests/MessageView.context.test.tsx
@@ -2,11 +2,11 @@ import { fireEvent, render } from '@testing-library/react';
 import { test } from 'vitest';
 import App from '../App';
 
-test('basic-usage-with-prefix', () => {
+test('the prefix in whatsapp UI should change to 123', () => {
   const { getByText, getByPlaceholderText } = render(<App />);
-  const inputPrefix = getByPlaceholderText('קידומת');
-  expect(getByText(/972/).textContent).toContain('972');
+
   // expect the '972' in the whatsapp UI will change, after we will change the ptefix
-  fireEvent.change(inputPrefix, { target: { value: '123' } });
+  expect(getByText(/972/).textContent).toContain('972');
+  fireEvent.change(getByPlaceholderText('קידומת'), { target: { value: '123' } });
   expect(getByText(/123/).textContent).toContain('123');
 });

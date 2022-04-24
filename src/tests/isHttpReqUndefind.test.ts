757d695d060c3a45bebb8421d51542ed0f4236eb
AviadSofer <aviadsofer@gmail.com>
2022-04-24 23:56:57 +0300

    update explaination in isHttpReqUndefind.test.ts

--- src/tests/isHttpReqUndefind.test.ts
+++ src/tests/isHttpReqUndefind.test.ts
@@ -1,24 +1,24 @@
 import { expect } from 'chai';
 import { isHttpReqUndefind } from '../controllers/signUpValidation';
 
-describe('should pass only if there no empty arguments', () => {
+describe('check if there no empty arguments', () => {
   const someText = 'something';
   const nothing = '';
 
-  it('should pass because everything is Ok', () => {
+  it('should return mail, because everything is Ok', () => {
     const mail = isHttpReqUndefind(someText, someText, someText);
     expect(mail).to.equal(someText);
   });
 
-  it('should fail because mail is empty', () => {
+  it('should throw error, because mail is empty', () => {
     expect(() => isHttpReqUndefind(nothing, someText, someText)).to.throw();
   });
 
-  it('should fail because userName is empty', () => {
+  it('should throw error, because userName is empty', () => {
     expect(() => isHttpReqUndefind(someText, nothing, someText)).to.throw();
   });
 
-  it('should fail because password is empty', () => {
+  it('should throw error, because password is empty', () => {
     expect(() => isHttpReqUndefind(someText, someText, nothing)).to.throw();
   });
 });

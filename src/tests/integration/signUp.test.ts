17b5efc998e1c8124878d2be638b8d38dabbce5a
AviadSofer <aviadsofer@gmail.com>
2022-05-03 10:16:32 +0300

    try to fix tsynce problum with integration test

--- src/tests/integration/signUp.test.ts
+++ src/tests/integration/signUp.test.ts
@@ -25,15 +25,15 @@ describe('creating new user', () => {
     const res = await request(app).post('/api/signup').send(user);
     expect(res.statusCode).to.equal(201);
     expect(res.body).to.include(`success, user:${user.userName} created :)`);
-  });
+  }).timeout(10000);
 
   it('try to create empty user, should return 400', async () => {
     const res = await request(app).post('/api/signup').send();
     expect(res.statusCode).to.equal(400);
-  });
+  }).timeout(10000);
 
   it('try to create the same user, should return 409', async () => {
     const res = await request(app).post('/api/signup').send(user);
     expect(res.statusCode).to.equal(409);
-  });
+  }).timeout(10000);
 });

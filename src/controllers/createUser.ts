18de177236d010522ea14955d6b3dc8ac3836269
AviadSofer <aviadsofer@gmail.com>
2022-05-04 22:25:13 +0300

    add hashed password

--- src/controllers/createUser.ts
+++ src/controllers/createUser.ts
@@ -1,13 +1,17 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 import { User, IUser } from '../models/User';
 
 const createUser = async (mail: string, userName: string, password: string) => {
+  // bcrypt password
+  const hashedPassword = await bcrypt.hash(password, 10);
+
   // creating new user
   const user = new User<IUser>({
     _id: new mongoose.Types.ObjectId(),
     mail,
     userName,
-    password,
+    password: hashedPassword,
   });
 
   // save it to the DB

d8dc533fdaee7dcebefb66f95e78a04370c69dc5
AviadSofer <aviadsofer@gmail.com>
2022-05-04 21:40:11 +0300

    add env variables to github action

--- src/helpers/dbConnect.ts
+++ src/helpers/dbConnect.ts
@@ -2,11 +2,9 @@ import mongoose from 'mongoose';
 import 'dotenv/config';
 import logger from '../logger/logger';
 
-const url = 'mongodb+srv://admin:1cfC1wzKOn53ZtJc@cluster0.9wxyh.mongodb.net/quick-message-for-whatsapp?retryWrites=true&w=majority';
-
 const dbConnect = async () => {
   try {
-    await mongoose.connect(url);
+    await mongoose.connect(`${process.env.MONGO_URL}`);
     logger.info('MongoDB connected');
   } catch (err) {
     logger.error('Failed to connect to MongoDB', err);

59c6f55ea7ab77cfaf5a13fcf27714cc36813ac9
AviadSofer <aviadsofer@gmail.com>
2022-05-04 21:27:06 +0300

    tro to fix ci/cd

--- src/helpers/dbConnect.ts
+++ src/helpers/dbConnect.ts
@@ -2,9 +2,11 @@ import mongoose from 'mongoose';
 import 'dotenv/config';
 import logger from '../logger/logger';
 
+const url = 'mongodb+srv://admin:1cfC1wzKOn53ZtJc@cluster0.9wxyh.mongodb.net/quick-message-for-whatsapp?retryWrites=true&w=majority';
+
 const dbConnect = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGO_URL}`);
+    await mongoose.connect(url);
     logger.info('MongoDB connected');
   } catch (err) {
     logger.error('Failed to connect to MongoDB', err);
@@ -12,7 +14,11 @@ const dbConnect = async () => {
 };
 
 const dbClose = async () => {
-  await mongoose.connection.close();
+  try {
+    await mongoose.connection.close();
+  } catch (err) {
+    logger.error(err);
+  }
 };
 
 export { dbConnect, dbClose };

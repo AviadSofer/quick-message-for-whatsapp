6af9f48a29948f7d10fda31123376e610fac75bc
AviadSofer <aviadsofer@gmail.com>
2022-05-08 16:24:31 +0300

    the ci should pass

--- src/tests/integration/signIn.test.ts
+++ src/tests/integration/signIn.test.ts
@@ -33,7 +33,7 @@ describe('login with user', () => {
     const res = await request(app).post('/api/signin').send({ userName, password });
     expect(res.statusCode).to.equal(200);
     // should verify and not fail
-    jwt.verify(res.body.token, `r${process.env.JWT_KEY}`);
+    jwt.verify(res.body.token, `${process.env.JWT_KEY}`);
   });
 
   it('incorrect userName, should return 401', async () => {

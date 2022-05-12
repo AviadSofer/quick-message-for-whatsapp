8127dbe1a9e408ef57a368c9735ad652019fc9ec
AviadSofer <aviadsofer@gmail.com>
2022-05-12 16:00:48 +0300

    order the get-message Post. add date from JS object. add userName from auth middlewere. add logger report. add try/catch

--- src/routes/api/getMessages.ts
+++ src/routes/api/getMessages.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response, Router } from 'express';
-import Message from '../../models/Message';
+import mongoose from 'mongoose';
+import logger from '../../logger/logger';
+import { Message, IMessage } from '../../models/Message';
 
 const router: Router = express.Router();
 
@@ -9,15 +11,28 @@ router.get('/', async (req: Request, res: Response) => {
 });
 
 router.post('/', async (req: Request, res: Response) => {
-  const { date, number, message } = req.body;
-  Message.create({
-    date,
-    number,
-    message,
-  }, (err) => {
-    if (err) throw err;
-    res.json(`${req.body.number} saved:)`);
-  });
+  const { decoded, phoneNumber, textMessage } = req.body;
+
+  try {
+    // creating new message with Message schema
+    const message = new Message<IMessage>({
+      _id: new mongoose.Types.ObjectId(),
+      date: new Date(),
+      userName: decoded.user.userName,
+      phoneNumber,
+      textMessage,
+    });
+    // save it in the DB
+    await message.save();
+    // send to the client
+    res.status(201).json({
+      message: 'message saved :)',
+    });
+    logger.info(`message from user:${decoded.user.userName} saved`);
+  } catch (err) {
+    res.send(err);
+    logger.error(err);
+  }
 });
 
 export default router;

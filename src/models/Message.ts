a874ef2a1222812f8d4baebc195d7e2199724bb5
AviadSofer <aviadsofer@gmail.com>
2022-05-12 14:20:10 +0300

    add interface for message. add required to chema:

--- src/models/Message.ts
+++ src/models/Message.ts
@@ -2,11 +2,20 @@ import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
 
-const messageSchema = new Schema({
-  userName: String,
-  date: String,
-  phoneNumber: String,
-  message: String,
+interface IMessage {
+  _id: mongoose.Types.ObjectId;
+  userName: string;
+  date: string;
+  phoneNumber: string;
+  message: string;
+}
+
+const messageSchema = new Schema<IMessage>({
+  _id: { type: mongoose.Schema.Types.ObjectId, required: true },
+  userName: { type: String, required: true },
+  date: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+  message: { type: String, required: true },
 });
 
 const Message = mongoose.model('messages', messageSchema);

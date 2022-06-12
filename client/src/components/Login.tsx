8fedf151ac2a514674b39ea914eb942da2879077
AviadSofer <aviadsofer@gmail.com>
2022-06-13 00:25:21 +0300

    build basic login page

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,8 +1,19 @@
-import StyledLogin from './styles/Login.styled';
+import {
+  StyledLogin, LoginContainer, LoginLogo, LoginTitle, ToSignUp, LoginButton,
+} from './styles/Login.styled';
+import logo from '../logo.png';
+import StyledInput from './styles/TextField.styled';
 
 const Login: React.FC = () => (
   <StyledLogin>
-    Basic login
+    <LoginContainer>
+      <LoginLogo src={logo} />
+      <LoginTitle>כניסה</LoginTitle>
+      <ToSignUp>אין לכם חשבון? הרשמה</ToSignUp>
+      <StyledInput width="70%" fontSize="0.7rem" placeholder="שם משתמש" />
+      <StyledInput width="70%" fontSize="0.7rem" placeholder="סיסמה" />
+      <LoginButton green>כניסה</LoginButton>
+    </LoginContainer>
   </StyledLogin>
 );
 
--- client/src/components/styles/Login.styled.ts
+++ client/src/components/styles/Login.styled.ts
@@ -1,7 +1,48 @@
 import styled from 'styled-components';
+import StyledButton from './Button.styled';
 
-const StyledLogin = styled.div`
+export const StyledLogin = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+background-color: #F5F5F5;
 flex: 2;
 `;
 
-export default StyledLogin;
+export const LoginContainer = styled.div`
+width: 30vw;
+height: 70vh;
+padding: 30px;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+align-items: center;
+background-color: white;
+border-style: solid;
+border-width: 1px;
+border-color: #DCDCDC;
+border-radius: 5px;
+box-shadow: 0px 0px 8px 4px #E0E0E0;
+`;
+
+export const LoginLogo = styled.img`
+width: 7vw;
+object-fit: contain;
+`;
+
+export const LoginTitle = styled.span`
+font-size: 1.1rem;
+font-family: Arial, Helvetica, sans-serif;
+`;
+
+export const ToSignUp = styled.span`
+font-size: 0.6rem;
+font-family: Arial, Helvetica, sans-serif;
+`;
+
+export const LoginButton = styled(StyledButton)`
+&& {
+  width: 70%;
+  font-size: 0.7rem;
+}
+`;

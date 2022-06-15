9ef978ec759ffc73ba2750099cfbea37920dcaf8
AviadSofer <aviadsofer@gmail.com>
2022-06-15 14:28:51 +0300

    add logout handle function

--- client/src/components/LoggedNavBar.tsx
+++ client/src/components/LoggedNavBar.tsx
@@ -1,17 +1,21 @@
 import {
-  AuthButtons, Logo, Nav, NavButton, StyledLink,
+  AuthButtons, Logo, Nav, NavButton,
 } from './styles/NavBar.styled';
 import logo from '../logo.png';
 
-const LoggedNavBar: React.FC = () => (
-  <Nav>
-    <Logo src={logo} />
-    <AuthButtons>
-      <StyledLink to="/signup">
-        <NavButton green={+true}>יציאה</NavButton>
-      </StyledLink>
-    </AuthButtons>
-  </Nav>
-);
+const LoggedNavBar: React.FC = () => {
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    window.location.href = '/';
+  };
+  return (
+    <Nav>
+      <Logo src={logo} />
+      <AuthButtons>
+        <NavButton green={+true} onClick={handleLogout}>יציאה</NavButton>
+      </AuthButtons>
+    </Nav>
+  );
+};
 
 export default LoggedNavBar;

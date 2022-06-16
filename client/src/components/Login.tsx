6884556484b6ea1336f9d80e25368c410ab21fec
AviadSofer <aviadsofer@gmail.com>
2022-06-16 18:03:04 +0300

    change the functions in Login to arr functions

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -9,32 +9,32 @@ const Login: React.FC = () => {
   const [userName, setUserName] = useState('');
   const [password, setPasswoed] = useState('');
   const [showErr, setShowErr] = useState(0);
+
   useEffect(() => {
     window.addEventListener('load', () => { // make the height static, useful with mobile keyboard
       const viewport = (document.querySelector('meta[name=viewport]') as HTMLMetaElement);
       viewport.setAttribute('content', `${viewport.content}, height=${window.innerHeight}`);
     });
   });
-  async function loginUser(credentials: {userName: string, password: string}) {
-    return fetch('/api/signin', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(credentials),
-    })
-      .then((data) => data.json())
-      .then((data) => data.token);
-  }
-  async function handleSubmit() {
-    const token = await loginUser({ userName, password });
+
+  const getToken = async (credentials: {userName: string, password: string}) => fetch('/api/signin', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(credentials),
+  }).then((data) => data.json())
+    .then((data) => data.token);
+
+  const handleSubmit = async () => {
+    const token = await getToken({ userName, password });
     if (token) {
       localStorage.setItem('token', token);
       window.location.href = '/';
     } else {
       setShowErr(+true);
     }
-  }
+  };
   return (
     <StyledLogin>
       <LoginContainer>

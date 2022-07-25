7b4c7f7001a67bb17d11296ba4d9ae697e488b94
AviadSofer <aviadsofer@gmail.com>
2022-07-25 22:16:06 +0300

    change <span> to StyledHelloUser in louding in HelloUser

--- client/src/components/HelloUser.tsx
+++ client/src/components/HelloUser.tsx
@@ -4,8 +4,8 @@ import StyledHelloUser from './styles/HelloUser.styled';
 
 const HelloUser: React.FC = () => {
   const { data, error } = useSWR('/api/get-user-profile', fetchData);
-  if (error) return <span>שגיאה :(</span>;
-  if (!data) return <span>בטעינה...</span>;
+  if (error) return <StyledHelloUser>שגיאה :(</StyledHelloUser>;
+  if (!data) return <StyledHelloUser>בטעינה...</StyledHelloUser>;
   const firstName = `${data.fullName}`.split(' ')[0];
   return (
     <StyledHelloUser>

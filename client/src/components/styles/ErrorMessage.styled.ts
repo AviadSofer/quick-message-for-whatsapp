ffb2eed864e5d82d0125a5b5d661352743490443
AviadSofer <aviadsofer@gmail.com>
2022-08-03 21:53:30 +0300

    responsive error message

--- client/src/components/styles/ErrorMessage.styled.ts
+++ client/src/components/styles/ErrorMessage.styled.ts
@@ -8,10 +8,10 @@ interface ErrorMessageProps {
 const ErrorMessage = styled.span<ErrorMessageProps>`
 display: ${({ showErr }) => (showErr ? 'inline' : 'none')};
 grid-area: ${({ gridarea }) => gridarea};
-width: 100%;
+width: 145%;
 align-self: flex-start;
 color: #f02849;
-font-size: 0.55rem;
+font-size: 0.6rem;
 font-family: Arial, Helvetica, sans-serif;
 `;
 

b63257fa17f425ed6b23730f1c1b7c856227a193
AviadSofer <aviadsofer@gmail.com>
2022-08-24 21:43:06 +0300

    delete foo()

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,29 +1,25 @@
 import { SocialIconsContainer, StyledFooter, StyledSocialIcon } from './styles/Footer.styled';
 import { ATitle, SmallTitle } from './styles/Title.styled';
 
-const Footer: React.FC = () => {
-  const foo = () => true;
-  foo();
-  return (
-    <StyledFooter>
-      <SmallTitle>
-        נוצר על ידי Aviad Sofer
-        |
-      </SmallTitle>
-      <SocialIconsContainer>
-        <StyledSocialIcon url="https://twitter.com/AviadSofer" />
-        <StyledSocialIcon url="https://github.com/AviadSofer" />
-        <StyledSocialIcon url="https://www.linkedin.com/in/aviad-sofer-03ba1818b" />
-      </SocialIconsContainer>
-      <SmallTitle>
-        |
-        {' '}
-        <ATitle href="https://github.com/AviadSofer/quick-message-for-whatsapp">
-          בקוד פתוח
-        </ATitle>
-      </SmallTitle>
-    </StyledFooter>
-  );
-};
+const Footer: React.FC = () => (
+  <StyledFooter>
+    <SmallTitle>
+      נוצר על ידי Aviad Sofer
+      |
+    </SmallTitle>
+    <SocialIconsContainer>
+      <StyledSocialIcon url="https://twitter.com/AviadSofer" />
+      <StyledSocialIcon url="https://github.com/AviadSofer" />
+      <StyledSocialIcon url="https://www.linkedin.com/in/aviad-sofer-03ba1818b" />
+    </SocialIconsContainer>
+    <SmallTitle>
+      |
+      {' '}
+      <ATitle href="https://github.com/AviadSofer/quick-message-for-whatsapp">
+        בקוד פתוח
+      </ATitle>
+    </SmallTitle>
+  </StyledFooter>
+);
 
 export default Footer;

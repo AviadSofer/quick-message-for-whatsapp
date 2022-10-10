98795d1bd12facbbec78234adebe1b3bae46cfd2
AviadSofer <aviadsofer@gmail.com>
2022-10-10 20:29:33 +0300

    ok, REMOVE env for clientID, it's not necessary in the Client

--- client/src/components/LoggedNavBar.tsx
+++ client/src/components/LoggedNavBar.tsx
@@ -12,7 +12,7 @@ interface Props {
 }
 
 const LoggedNavBar: React.FC<Props> = ({ handleModal }) => {
-  const clientId = `${import.meta.env.VITE_CLIENT_ID}`;
+  const clientId = '27378581193-0oui5pt2qm345p1u41gpim0gbnvsm5jm.apps.googleusercontent.com';
 
   const handleLogout = async () => {
     await logout();
